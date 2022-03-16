e202324c092297af45225800c60e1d081ff5cf8b
Mathieu Albin <mathieualbin@MacBook-Air-de-Mathieu.local>
2022-03-16 20:22:30 +0100

    ajout CORS CREDENTIALS

--- mySearchEngine/mySearchEngine/settings.py
+++ mySearchEngine/mySearchEngine/settings.py
@@ -55,12 +55,12 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
 ]
 
@@ -138,3 +138,5 @@ CORS_ORIGIN_WHITELIST = [
 'http://localhost:4200',
 'http://127.0.0.1:8000'
 ]
+
+CORS_ALLOW_CREDENTIALS = True
